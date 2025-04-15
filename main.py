132df81c0d20c9a66f180cac0579277349402ddf
nihaltp <nihaltpnki@gmail.com>
2025-04-15 09:20:40 +0530

    Refactor N Queens solver for clarity and functionality improvements, add board size input

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 
 """
-    8 Queens solver
+    N Queens solver
     
     Made by: NIHAL T P
     GitHub: https://github.com/nihaltp
@@ -12,7 +12,6 @@ import os
 os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "1"
 
 import sys
-import random
 from time import sleep
 
 try:
@@ -30,110 +29,98 @@ class solver:
             print(f"Error initializing Pygame: {e}")
             sys.exit(1)
         
-        self.board_size : int = 8
+        self.board_size   : int = 8  # for making the inital window
         self.square_width : int = 50
-        self.screen_size : int = self.square_width * self.board_size + 2 * self.square_width
-        
-        # Create an empty 8x8 matrix
-        # when representing the queens the value is the column number
-        # row is the index of the list
-        self.board : list = [-1 for _ in range(self.board_size)]
+        self.screen_size  : int = self.square_width * self.board_size + 2 * self.square_width
         
         # Colors
-        self.background : tuple = (135, 206, 250)
-        self.yellow : tuple = (255, 255, 0)
-        self.white : tuple = (255, 255, 255)
-        self.black : tuple = (0, 0, 0)
+        self.BACKGROUND : tuple = (135, 206, 250)
+        self.WHITE      : tuple = (255, 255, 255)
+        self.BLACK      : tuple = (0, 0, 0)
+        
+        # Create the Pygame window for Getting Board Size
+        self.screen = pygame.display.set_mode((self.screen_size, self.screen_size//4))
+        pygame.display.set_caption("N-Queens Solver Board Size")
+        
+        # Pygame font setup
+        pygame.font.init()
+        self.FONT = pygame.font.SysFont(None, 28)
+        
+        self.board_size = self.input_text("Size of the board (N x N): ", (self.square_width, self.square_width), 12)
+        self.screen_size = self.square_width * self.board_size + 2 * self.square_width  # recalculate the screen size based on the new board_size
+        
+        # Create an empty 8x8 matrix
+        # when representing the queens:
+            # row is the index of the list
+            # and the value is the column number
+        self.board   : list = [-1 for _ in range(self.board_size)]
+        self.answers : list = []
         
         # Board positioning
         self.board_x : int = (self.screen_size - self.board_size * self.square_width) // 2
         self.board_y : int = (self.screen_size - self.board_size * self.square_width) // 2
         
-        # Create the Pygame window
+        # Create the Pygame window for Queens Solver
         self.screen = pygame.display.set_mode((self.screen_size, self.screen_size))
         pygame.display.set_caption(f"{self.board_size} Queens Solver")
-        
-        # Pygame font setup
-        pygame.font.init()
-        self.font = pygame.font.SysFont(None, 28)
         
         self.queen_b = pygame.image.load("pieces/queen_b.png").convert_alpha()
         self.queen_w = pygame.image.load("pieces/queen_w.png").convert_alpha()
         self.queen_b = pygame.transform.scale(self.queen_b, (self.square_width, self.square_width))
         self.queen_w = pygame.transform.scale(self.queen_w, (self.square_width, self.square_width))
         
-        self.delay : int = 0.01
+        self.delay : float = 0.01
         
-        self.screen.fill(self.background)  # draw the background
+        self.screen.fill(self.BACKGROUND)  # draw the background
         self.game()
     
     # MARK: game
     def game(self) -> None:
-        # Game loop
-        self.queen_placement()
-    
-    # MARK: queen_placement
-    def queen_placement(self, column: int = 0) -> None:
-        print("\033[92mStarting the game...\033[0m")
-        for col in range(self.board_size):
-            if col < column:
-                continue
-            if self.board[col] == -1:
-                self.board[col] = 0  # Start from the first row
-            self.draw_board()
-            self.handle_events()
+        print(f"\033[92mStarting the {self.board_size}-Queens solver...\033[0m")
+        col: int = 0
+        self.draw_board()
+        
+        while col >= 0:
+            self.board[col] += 1
             
-            # Check if the queen placement is valid
-            while not self.is_valid_placement():
-                self.board[col] += 1  # Move to the next row
-                print(f"Incrementing row \033[93m{col}\033[0m to \033[94m{self.board[col]}\033[0m")
-                
-                if self.board[col] >= self.board_size:
-                    # Backtrack to the previous column
-                    print(f"\033[91mBacktracking by resetting \033[93m{col}\033[91m and incrementing \033[93m{col - 1}\033[0m")
-                    self.board[col] = -1  # Reset the current column
-                    self.board[col - 1] += 1  # On the previous row, Move to the next column
-                    self.queen_placement(col - 1)  # Recursive call to the previous column
-                    return
-                
+            while self.board[col] < self.board_size and not self.is_valid_placement(col):
+                # if the placement is not valid, move the queen to the next column
+                self.board[col] += 1
                 self.draw_board()
                 self.handle_events()
             
-            print(f"\033[92m{self.board}\033[0m")
+            if self.board[col] < self.board_size:
+                # if the queen is within bounds
+                if col == self.board_size - 1:
+                    self.answers.append(self.board.copy())
+                    print(f"\033[92mSolution found \033[94m({len(self.answers)})\033[92m: \033[93m{self.board}\033[0m")
+                    self.draw_board()
+                    sleep(3) # Pause to show the solution
+                    self.handle_events()
+                else:
+                    col += 1              # Move to the next column
+                    self.board[col] = -1  # Reset the next column
+            else:
+                self.board[col] = -1  # Reset the current column
+                col -= 1              # Backtrack to the previous column
+            
+            self.draw_board()
+        
+        print(f"\033[92mAll solutions found \033[94m({len(self.answers)})\033[92m: \033[93m{self.answers}\033[0m")
     
     # MARK: is_valid_placement
-    def is_valid_placement(self) -> bool:
+    def is_valid_placement(self, current_col: int) -> bool:
+        # [] Mark X on the squares that are not usable
         """Check if the current queen placement is valid.
         
         Returns:
             bool: True if the placement is valid, False otherwise.
         """
         
-        for i in range(self.board_size):
-            # Check if the queen is placed
-            if self.board[i] == -1:
-                print(f"\033[91mQueen not placed in \033[93mrow: \033[94m{i}\033[0m")
-                return True
-            
-            if self.board[i] >= self.board_size:
-                print(f"\033[91mQueen out of bounds in \033[93mrow: \033[94m{i}\033[0m")
+        for i in range(current_col):
+            if (self.board[i] == self.board[current_col] or
+                abs(self.board[i] - self.board[current_col]) == abs(i - current_col)):
                 return False
-            
-            # Check if the queen is in the same row
-            for j in range(self.board_size):
-                if i == j:
-                    continue # Skip the same row
-                if self.board[j] == -1:
-                    break
-                if self.board[i] == self.board[j]:
-                    print(f"\033[91mQueens in same \033[93mcolumn: \033[94m{i} and {j}\033[0m")
-                    return False
-                
-                # Check if the queens are in the same diagonal
-                if abs(self.board[i] - self.board[j]) == abs(i - j):
-                    print(f"\033[91mQueens in same \033[95mdiagonal: \033[94m{i} and {j}\033[0m")
-                    return False
-        
         return True
     
     # MARK: draw_board
@@ -149,7 +136,7 @@ class solver:
     # MARK: draw_square
     def draw_square(self, row: int, column: int, queen: bool) -> None:
         """Draw individual sensor squares."""
-        color = self.white if ((row + column) % 2) == 0 else self.black
+        color: tuple = self.WHITE if ((row + column) % 2) == 0 else self.BLACK
         
         # Draw the square
         square_rect = pygame.Rect(
@@ -182,9 +169,42 @@ class solver:
                     self.delay /= 2
                     print(f"\033[93mDelay: {self.delay}\033[0m")
     
+    # MARK: input_text
+    def input_text(self, prompt: str, position: tuple, limit: int) -> int:
+        input_active: bool = True
+        input_text: list = []
+        
+        while input_active:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.quit_game()
+                elif event.type == pygame.KEYDOWN:
+                    if event.unicode == "\r" and input_text:
+                        input_active = False
+                    elif event.key == pygame.K_BACKSPACE:
+                        input_text = input_text[:-1]
+                    elif len(input_text) < 2:  # Limit the length of the text
+                        input_text.append(event.unicode)
+            
+            self.screen.fill(self.BACKGROUND)
+            
+            # Display the input text
+            current_text: str = ''.join(input_text)
+            value: str = f"{prompt} {current_text}"
+            text = self.FONT.render(value, True, self.BLACK)
+            text_rect = text.get_rect(topleft = position)
+            self.screen.blit(text, text_rect)
+            pygame.display.flip()
+        
+        current_num: int = int(current_text)
+        while current_num > limit:
+            current_num = self.input_text(prompt + f" limit is {limit}", position, limit)
+        return current_num
+    
     # MARK: quit_game
     def quit_game(self) -> None:
         """Exit the Pygame window."""
+        print(f"\033[92mSolutions found \033[94m({len(self.answers)})\033[92m: \033[93m{self.answers}\033[0m")
         pygame.quit()
         sys.exit(0)
 
